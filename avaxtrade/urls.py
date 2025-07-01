4e372325005f7c832524e59f02a17c67756f6199
amorgrace <akanforte@gmail.com>
2025-07-01 22:25:08 +0100

    cronjob active

--- avaxtrade/urls.py
+++ avaxtrade/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({'status': '200'})
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('apiconfig.urls'))
+    path('api/auth/', include('apiconfig.urls')),
+    path('/ping', health_check )
+
 ]
