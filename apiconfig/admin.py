0beae0234675b206e41860831226037a3cc74042
amorgrace <akanforte@gmail.com>
2026-01-09 00:32:30 +0100

    kyc errror

--- apiconfig/admin.py
+++ apiconfig/admin.py
@@ -1,29 +1,34 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, RecentTransaction
+from .models import CustomUser, RecentTransaction, KYC
+
+class KYCInline(admin.TabularInline):  # or admin.StackedInline
+    model = KYC
+    extra = 1
+    fields = ['kyc_status', 'image_url', 'created_at']
+    readonly_fields = ['created_at']
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     ordering = ['email']
-
     list_display = ('email', 'fullname', 'main', 'profit', 'is_staff', 'is_active', 'is_superuser')
-
+    
     fieldsets = (
         (None, {'fields': ('email', 'password', 'fullname')}),
         ('Financial Info', {'fields': ('main', 'profit')}),
-        ('KYC Info', {'fields': ('kyc_status', 'kyc_photo')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important Dates', {'fields': ('last_login',)}),
     )
-
+    
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'fullname', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser'),
         }),
     )
-
-    # readonly_fields = ('total',)
+    
+    # Add inline for KYC records
+    inlines = [KYCInline]
 
 @admin.register(RecentTransaction)
 class RecentTransactionAdmin(admin.ModelAdmin):
